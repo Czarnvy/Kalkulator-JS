b60747664718e60baba7119868ae1398154c66df
Czarnvy <126722269+Czarnvy@users.noreply.github.com>
2023-03-25 19:19:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var button = document.querySelectorAll(".button");
 const screen = document.querySelector(".screen");
 const buttonsArray = [...button];
+var operation = "";
 
 const buttons = buttonsArray.map(btn => {
     btn.addEventListener("click", () => type(btn.textContent)) 
 });
 
 function type(content){
-    if(content == "CE")
-        screen.textContent = "";
-    else if(content == "=")
-        result();
-    else
+    if(content != "CE" && content != "="){
         screen.textContent += content;
-}
+        operation += content;
+    }
+    else if(content == "=")
+        result(operation);
+    else
+        location.reload();
+}   
 
+
+function result(operation){
+
+    var wynik = eval(operation);
+    screen.textContent = wynik;
+
+    console.log(operation);
+}
